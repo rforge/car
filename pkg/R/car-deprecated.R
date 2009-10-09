8bb11f84bfa0d1789ca8533a88fcb3aa0a520222
jfox <jfox@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-10-09 20:49:33 +0000

    moved ceres.plots, cr.plots, leverage.plots to art; added deprecated functions; fixed cross-references in .Rd files; renamed Can.pop and Us.pop data frames. John
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/car@15 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/car-deprecated.R
@@ -0,0 +1,96 @@
+# last modified 2009-10-09 by J. Fox
+
+av.plot <- function (...) {
+	.Deprecated("avPlot", package="art")
+	avPlot(...)
+}
+
+av.plots <- function (...) {
+	.Deprecated("avPlots", package="art")
+	avPlot(...)
+}
+
+box.cox <- function (...) {
+	.Deprecated("bcPower", package="art")
+	bcPower(...)
+}
+
+bc <- function (...) {
+	.Deprecated("bcPower", package="art")
+	bcPower(...)
+}
+
+box.cox.powers <- function (...) {
+	.Deprecated("powerTransform", package="art")
+	powerTransform(...)
+}
+
+box.cox.var <- function (...) {
+	.Deprecated("boxCoxVariable")
+	boxCoxVariable(...)
+}
+
+box.tidwell <- function (...) {
+	.Deprecated("boxTidwell", package="art")
+	boxTidwell(...)
+}
+
+ceres.plot <- function (...) {
+	.Deprecated("ceresPlot", package="art")
+	crPlot(...)
+}
+
+ceres.plots <- function (...) {
+	.Deprecated("ceresPlots", package="art")
+	crPlot(...)
+}
+
+cookd <- function (...) {
+	.Deprecated("cooks.distance", package="stats")
+	cooks.distance(...)
+}
+
+cr.plot <- function (...) {
+	.Deprecated("crPlot", package="art")
+	crPlot(...)
+}
+
+cr.plots <- function (...) {
+	.Deprecated("crPlots", package="art")
+	crPlot(...)
+}
+
+durbin.watson <- function (...) {
+	.Deprecated("durbinWatsonTest", package="art")
+	durbinWatsonTest(...)
+}
+
+levene.test <- function (...) {
+	.Deprecated("leveneTest", package="art")
+	leveneTest(...)
+}
+
+leverage.plot <- function (...) {
+	.Deprecated("leveragePlot", package="art")
+	leveragePlot(...)
+}
+
+leverage.plots <- function (...) {
+	.Deprecated("leveragePlots", package="art")
+	leveragePlots(...)
+}
+
+qq.plot <- function (...) {
+	.Deprecated("qqPlot")
+	qqPlot(...)
+}
+
+scatterplot.matrix <- function (...) {
+	.Deprecated("scatterplotMatrix")
+	scatterplotMatrix(...)
+}
+
+spread.level.plot <- function (...) {
+	.Deprecated("spreadLevelPlot")
+	spreadLevelPlot(...)
+}
