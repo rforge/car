a1d104b866494aafbfd1a493445d2521f1583192
jfox <jfox@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-02-03 21:17:35 +0000

    fixed carWeb(). John
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/car@79 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/carWeb.R
+++ pkg/R/carWeb.R
@@ -2,8 +2,8 @@ carWeb <-
 function (what = c("webpage", "errata", "taskviews"))
 {
     what = match.arg(what)
-    urls = c(webpage = "http://www.stat.umn.edu/alr/",
-        errata = "http://www.stat.umn.edu/alr/Links/errata.pdf",
+    urls = c(webpage = "http://socserv.socsci.mcmaster.ca/jfox/Books/Companion/",
+        errata = "http://socserv.socsci.mcmaster.ca/jfox/Books/Companion/errata.pdf",
         taskviews = "http://cran.r-project.org/web/views")
     url = urls[what]
     browseURL(url)
