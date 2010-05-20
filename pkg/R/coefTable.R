2eb15f2d2c780d9363b19351d24a0bc5944c7f2e
jfox <jfox@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-05-20 20:17:59 +0000

    small change to coefTable. John
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/car@132 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/coefTable.R
+++ pkg/R/coefTable.R
@@ -1,3 +1,5 @@
+# 20 May 2010: small changes to output when there is just one model. J. Fox 
+
 coefTable <- function(..., se=TRUE, digits=3){
     models <- list(...)
     n.models <- length(models)
@@ -6,14 +8,15 @@ coefTable <- function(..., se=TRUE, digits=3){
                          names(coef(model)))))
     table <- matrix(NA, length(coef.names), n.models*(1 + se))
     rownames(table) <- coef.names
-    colnames(table) <- if (se) paste(rep(c("Est.", "SE"), n.models),
-                           rep(1:n.models, each=2))
-        else paste(rep("Est.", n.models), 1:n.models)
+    colnames(table) <- if (se) if (n.models > 1) paste(rep(c("Est.", "SE"), n.models),
+                           rep(1:n.models, each=2)) else c("Estimate", "SE")
+        else if (n.models > 1) paste(rep("Est.", n.models), 1:n.models) else "Estimate"
     cat("\nCall:")
     for (i in 1:n.models){
         model <- models[[i]]
         fout <- deparse(model$call,width.cutoff=getOption("width") - 9)
-        cat(paste("\n",i,": ", fout[1], sep=""))
+		mod <- if (n.models > 1) paste(i, ":", sep="") else ""
+        cat(paste("\n", mod, fout[1], sep=""))
         if(length(fout) > 1) for (f in fout[-1]) cat("\n",f)
         if (se)
           table[names(coef(model)), 2*(i - 1) + c(1, 2)] <-
@@ -23,5 +26,3 @@ coefTable <- function(..., se=TRUE, digits=3){
     cat("\n")
     printCoefmat(table, na.print="", digits=digits, tst.ind=NULL)
 }
-
-
