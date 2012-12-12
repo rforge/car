4d36db4e34c91cfd5ccf973b7155fa8d96fcc9f2
jfox <jfox@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-12-12 16:01:48 +0000

    multiple conditioning vars for Boxplot.formula(). John
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/car@302 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/Boxplot.R
+++ pkg/R/Boxplot.R
@@ -1,5 +1,5 @@
 # checked in 26 December 2009 by J. Fox
-# 6 January 2009: Fixed Boxplot.default() so that it works properly when g is numeric. J. Fox
+# 2012-12-12: Fixed Boxplot.default() so that it works properly when g is numeric. J. Fox
 
 Boxplot <- function(y, ...){
 	UseMethod("Boxplot")
@@ -116,7 +116,10 @@ Boxplot.formula <- function(formula, data=NULL, subset, na.action=NULL, labels.,
 		response <- attr(attr(mf, "terms"), "response")
 		if (missing(ylab)) ylab <- names(mf)[response]
 		if (missing(xlab)) xlab <- names(mf)[-c(response, lab.var)]
-		Boxplot(mf[[response]], mf[, -c(response, lab.var)], labels=mf[[lab.var]], 
+        x <- mf[, -c(response, lab.var)]
+		if (is.data.frame(x)) x <- do.call("interaction", as.list(x))
+        if (length(xlab) > 1) xlab <- paste(xlab, collapse="*")
+		Boxplot(mf[[response]], x, labels=mf[[lab.var]], 
             xlab=xlab, ylab=ylab, id.method=id.method, ...)
 	}
 	else if (length(formula) == 2){
