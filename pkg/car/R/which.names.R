6b77eb30b5f8e5bd39bc6c61d0ae757dd8227ff8
jfox <jfox@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-01-28 18:44:58 +0000

    modified which.names()/whichNames(); restored NEWS for versions prior to 3.0-0
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/car@501 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/car/R/which.names.R
+++ pkg/car/R/which.names.R
@@ -1,13 +1,15 @@
 # positions of names in a data frame (J. Fox)
 
-# last modified 30 April 2010 by J. Fox
+# last modified 2018-01-28 by J. Fox
 
 which.names <- function(names, object){
-    row.names <- if (inherits(object, "data.frame")) row.names(object) else object
+    row.names <- if (inherits(object, "data.frame")) row.names(object) else names(object)
     check <- outer(row.names, names, '==')
     if (!all(matched <- apply(check, 2, any))) 
         warning(paste(paste(names[!matched], collapse=", "), "not matched"))
-    which(apply(check, 1, any))
+    result <- which(apply(check, 1, any))
+    names(result) <- row.names[result]
+    result
     }
 	
 whichNames <- function(...) which.names(...)
