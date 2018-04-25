a05b15c41342e7a0c99f9cfa0fd82fa0bc5b823f
jfox <jfox@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-04-25 17:32:22 +0000

    update carWeb()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/car@536 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/car/R/carWeb.R
+++ pkg/car/R/carWeb.R
@@ -6,6 +6,7 @@
 #                 download a script file if script = chap-num
 #  Add more to carWeb including cheat sheets
 # 2/21/2018 deleted "ethics" from the deafulat for page
+# 2018-04-25: J.fox. Update website URLs; update setup files
 
 
 carWeb <-
@@ -13,13 +14,15 @@ function (page = c("webpage", "errata", "taskviews"), script, data, setup)
 {
     rstudiocheat <- "https://www.rstudio.com/resources/cheatsheets/"
     ide.cheat <- "https://www.rstudio.com/wp-content/uploads/2016/01/rstudio-IDE-cheatsheet.pdf"
-    data.page <- "http://socserv.socsci.mcmaster.ca/jfox/Books/Companion/data/"
-    files <- c("Duncan.txt", "Hamlet.txt", "Prestige.txt", "Prestige-bugged.txt", "Datasets.xls")
-    script.page <- "http://socserv.socsci.mcmaster.ca/jfox/Books/Companion/scripts/"
+    data.page <- "https://socialsciences.mcmaster.ca/jfox/Books/Companion/data/"
+    setup.dir <- "https://socialsciences.mcmaster.ca/jfox/Books/Companion/setup/"
+    files <- c("Duncan.txt", "Duncan.csv", "Duncan.xlsx", "Duncan.Rmd", "Hamlet.txt", "RMarkdownTest.Rmd", 
+               "zipmod.R", "zipmodBugged.R", "zipmod-generic.R", paste0("chap-", 1:10, ".R"))
+    script.page <- "https://socialsciences.mcmaster.ca/jfox/Books/Companion/scripts/"
     ethics <- "http://www.amstat.org/asa/files/pdfs/EthicalGuidelines.pdf"
     page = match.arg(page)
-    urls = c(webpage = "http://socserv.socsci.mcmaster.ca/jfox/Books/Companion/",
-        errata = "http://socserv.socsci.mcmaster.ca/jfox/Books/Companion/errata.html",
+    urls = c(webpage = "https://socialsciences.mcmaster.ca/jfox/Books/Companion/",
+        errata = "https://socialsciences.mcmaster.ca/jfox/Books/Companion/errata.html",
         taskviews = "http://cran.r-project.org/web/views",
         ethics = ethics)
     url <- urls[page]
@@ -34,9 +37,9 @@ function (page = c("webpage", "errata", "taskviews"), script, data, setup)
        sfile <- paste(c(sfile, "R"), collapse="." )
        url <- paste(script.page, sfile, sep="")}
     if(!missing(setup)){
-      for(f in files) download.file(paste(data.page, f, sep=""),
+      for(f in files) download.file(paste(setup.dir, f, sep=""),
                                     paste(getwd(), f, sep="/"))
-      return(NULL)
+      return(invisible(NULL))
     }
     browseURL(url)
 }
